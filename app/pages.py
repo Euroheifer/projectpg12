8e1d991400cf52bd23c2809760c0bc6adc5e5c67
Euroheifer <122586110+Euroheifer@users.noreply.github.com>
2025-11-11 16:09:45 +0800

    v33

--- app/pages.py
+++ app/pages.py
@@ -4,30 +4,36 @@ from fastapi.templating import Jinja2Templates
 from fastapi.security import HTTPBearer
 from fastapi.responses import HTMLResponse
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="app/templates")
 security = HTTPBearer()
-router = APIRouter()
+pages_router = APIRouter()
 
+@pages_router.get("/", response_class=HTMLResponse)
 async def signup_page(request: Request):
     context = {"request": request, "title": "User Registration"}
     return templates.TemplateResponse("signup.html", context)
 
+@pages_router.get("/login", response_class=HTMLResponse)
 async def login_page(request: Request):
     context = {"request": request, "title": "User Login"}
     return templates.TemplateResponse("login.html", context)
 
+@pages_router.get("/home", response_class=HTMLResponse)
 async def home_page(request: Request):
     context = {"request": request, "title": "User Home"}
     return templates.TemplateResponse("home.html", context)
 
+@pages_router.get("/groups/{group_id}", response_class=HTMLResponse)
 async def get_group_page(request: Request, group_id: int):
     # Return unified group.html based on user permissions
     return templates.TemplateResponse("groups.html", {"request": request})
 
+@pages_router.get("/groups-admin", response_class=HTMLResponse)
 async def groups_admin_page(request: Request):
     context = {"request": request, "title": "User Interface (Admin)"}
     return templates.TemplateResponse("group_details_admin.html", context)
 
+@pages_router.get("/groups-member", response_class=HTMLResponse)
 async def groups_member_page(request: Request):
     context = {"request": request, "title": "User Interface (Member)"}
     return templates.TemplateResponse("group_details_member.html", context)
